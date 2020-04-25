6f94e8e9b894ecd3932b907141a0a00742c72e11
Chase Yang <chase@cyanbluecreative.com>
2020-04-25 17:54:50 -0500

    Create simple under construction page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        CYAN BLUE CREATIVE
+        <h1><b>CYAN BLUE CREATIVE</b></h1>
+
+        <small>☕ Currently Undergoing Upgrades ☕</small>
       </header>
     </div>
   );
