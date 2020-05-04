f03340423b34c8f241699b26621c990c18312ac0
Chase Yang <chase@cyanbluecreative.com>
2020-05-03 21:06:47 -0500

    Setup basic gallery + toggle construction page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import ImgGallery from './components/imgGallery';
+import Header from './components/header';
+import Footer from './components/footer';
 
-function App() {
+const App = () => {
+  const [message, toggleMessage] = useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1><b>CYAN BLUE CREATIVE</b></h1>
+    <div>
+      {message ? (
+        <div className="App">
+          <header className="App-header">
+            <h1>
+              <b>CYAN BLUE CREATIVE</b>
+            </h1>
 
-        <small>☕ Currently Undergoing Upgrades ☕</small>
-      </header>
+            <small>☕ Currently Undergoing Upgrades ☕</small>
+          </header>
+        </div>
+      ) : (
+        <div style={{ margin: '5px' }}>
+          <Header />
+          <ImgGallery />
+        </div>
+      )}
+      <div style={{ textAlign: 'center', marginTop: '15px' }}>
+        <button onClick={() => toggleMessage(!message)}>♥️</button>
+        <Footer />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
