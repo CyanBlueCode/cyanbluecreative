03132af1e5d2ba6d5c27cd43c05843c52910f3fe
Chase Yang <chase@cyanbluecreative.com>
2020-05-14 20:59:16 -0500

    Revert "Firebase basic setup"
    This reverts commit 06c875a34452b9ed7a512325f4387710dad4fb0a.

--- NOTES.md
+++
@@ -1,2 +0,0 @@
-react-lazyload
-https://www.freecodecamp.org/news/how-to-optimize-react-applications-with-lazy-loading-232183e02768/
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import ImgGallery from './components/imgGallery';
 import Header from './components/header';
@@ -7,22 +7,9 @@ import Navigation from './components/navigation';
 import HomePage from './components/homePage';
 import UnderConstruction from './components/underConstruction';
 import { Switch, Route, withRouter } from 'react-router-dom';
-import Firebase from './config';
 
 const App = () => {
   const [message, toggleMessage] = useState(false);
-  const [test, setTest] = useState('not working');
-
-  const database = Firebase.database().ref('images').child('test')
-
-  useEffect(() => {
-    database.on('value', (snap) => {
-      setTest(snap.val());
-    });
-  }, []);
-
-  console.log('=>', test);
-
   return (
     <div>
       {/* {message ? ( */}
